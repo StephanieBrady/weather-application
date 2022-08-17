4e45d9ebba41fd87fce84f690dcb4c8d4b4b6ae9
StephanieBrady <stephaniebrady1929@gmail.com>
2022-08-17 09:46:29 -0400

    removed HTML for forecast. updated js and injected into html

--- src/index.js
+++ src/index.js
@@ -35,42 +35,30 @@ let searchForm = document.querySelector(".form-inline");
 searchForm.addEventListener("submit", search);
 
 //
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
+  let days = ["Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"];
 
   let forecastHTML = `<div class="row">`;
-  forecastHTML =
-    forecastHTML +
-    `<div class="container-fluid">
-            <div class="col-2">
-              <div class="weather-forecast" id="forecast">
-                <div class="forecast-date"><strong>Sunday </strong></div>
-                <img
-                  src="http://openweathermap.org/img/wn/10d@2x.png"
-                  alt=""
-                  width="42"
-                />
-              </div>
-            </div>
-          </div>
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2 ">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
         </div>
-      </div>`;
-  forecastHTML =
-    forecastHTML +
-    `<div class="container-fluid">
-            <div class="col">
-              <div class="weather-forecast" id="forecast">
-                <div class="forecast-date"><strong>Sunday </strong></div>
-                <img
-                  src="http://openweathermap.org/img/wn/10d@2x.png"
-                  alt=""
-                  width="42"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>`;
+      </div>
+  `;
+  });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
