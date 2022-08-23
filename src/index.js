44225b87a1ae8c8211464abfb56ab4b4dfd99461
StephanieBrady <stephaniebrady1929@gmail.com>
2022-08-23 11:31:48 -0400

    made forecast responsive for small screens

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-      <div class="col-2 ">
+      <div class="col-sm-2 ">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div> 
       
         <img
